f498bc614f3cd2990c99ed11a264993994ee7866
hamza-nafasat <hamzanafasat4155@gmail.com>
2024-08-23 14:27:49 +0500

    add latest code

--- src/pages/dashboard/settings/devices/components/AddDevice.jsx
+++ src/pages/dashboard/settings/devices/components/AddDevice.jsx
@@ -1,5 +1,17 @@
 /* eslint-disable react/prop-types */
-import { Box, Button, Grid, TextField, Typography, styled } from "@mui/material";
+import {
+  Box,
+  Button,
+  FormControl,
+  FormHelperText,
+  Grid,
+  InputLabel,
+  MenuItem,
+  Select,
+  TextField,
+  Typography,
+  styled,
+} from "@mui/material";
 import { useFormik } from "formik";
 import { Fragment } from "react";
 import BackIcon from "../../../../../assets/svgs/modal/BackIcon";
@@ -10,182 +22,212 @@ import { useDispatch } from "react-redux";
 import { addDeviceAction } from "../../../../../redux/actions/device.actions";
 
 const AddDevice = ({ onClose }) => {
-    const dispatch = useDispatch();
-    const initialValues = {
-        deviceName: "",
-        deviceType: "",
-        ipAddress: "",
-        uniqueId: "",
-    };
+  const dispatch = useDispatch();
+  const initialValues = {
+    deviceName: "",
+    deviceType: "",
+    ipAddress: "",
+    uniqueId: "",
+    url: "",
+  };
 
-    const { values, touched, errors, handleBlur, handleChange, handleSubmit } = useFormik({
-        initialValues,
-        validationSchema: addDeviceSchema,
-        onSubmit: (values) => {
-            const data = {
-                name: values.deviceName,
-                type: values.deviceType,
-                ip: values.ipAddress,
-                uniqueId: values.uniqueId,
-            };
-            dispatch(addDeviceAction(data));
-        },
-    });
+  const { values, touched, errors, handleBlur, handleChange, handleSubmit } = useFormik({
+    initialValues,
+    validationSchema: addDeviceSchema,
+    onSubmit: (values) => {
+      const data = {
+        name: values.deviceName,
+        type: values.deviceType,
+        ip: values.ipAddress,
+        uniqueId: values.uniqueId,
+      };
+      if (values.deviceType == "video") {
+        data.url = values.url;
+      }
+      dispatch(addDeviceAction(data));
+    },
+  });
 
-    return (
-        <Fragment>
-            <Box
-                sx={{
-                    display: "flex",
-                    alignItems: "center",
-                    justifyContent: "space-between",
-                }}
-            >
-                <Box
-                    sx={{
-                        display: "flex",
-                        alignItems: "center",
-                        gap: "1rem",
-                        color: "rgba(17, 17, 17, 1)",
-                        fontSize: {
-                            xs: "1rem",
-                            md: "1.5rem",
-                        },
-                        fontWeight: 600,
-                    }}
+  return (
+    <Fragment>
+      <Box
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "space-between",
+        }}
+      >
+        <Box
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            gap: "1rem",
+            color: "rgba(17, 17, 17, 1)",
+            fontSize: {
+              xs: "1rem",
+              md: "1.5rem",
+            },
+            fontWeight: 600,
+          }}
+        >
+          <Box sx={{ cursor: "pointer", height: "25px" }} onClick={onClose}>
+            <BackIcon />
+          </Box>
+          ADD DEVICE
+        </Box>
+        <Box sx={{ cursor: "pointer" }} onClick={onClose}>
+          <CloseIcon onClick={onClose} />
+        </Box>
+      </Box>
+      {/* Form  */}
+      <Box
+        sx={{
+          marginTop: {
+            xs: "1rem",
+            lg: "2.5rem",
+          },
+        }}
+      >
+        <Typography
+          sx={{
+            fontWeight: 700,
+            fontSize: "20px",
+            marginBottom: "2rem",
+          }}
+        >
+          General Info
+        </Typography>
+        <form onSubmit={handleSubmit}>
+          <Grid container spacing="14">
+            <Grid item xs="12" lg="6">
+              <TextField
+                fullWidth
+                type="text"
+                name="deviceName"
+                value={values.deviceName}
+                onBlur={handleBlur}
+                onChange={handleChange}
+                error={touched.deviceName && Boolean(errors.deviceName)}
+                helperText={touched.deviceName && errors.deviceName}
+                label="Device Name"
+                maxLength="20"
+              />
+            </Grid>
+            <Grid item xs={12} lg={6}>
+              <FormControl fullWidth error={touched.deviceType && Boolean(errors.deviceType)}>
+                <InputLabel
+                  sx={{ backgroundColor: "whitesmoke", width: "fit-content", px: 1 }}
+                  id="deviceType-label"
                 >
-                    <Box sx={{ cursor: "pointer", height: "25px" }} onClick={onClose}>
-                        <BackIcon />
-                    </Box>
-                    ADD DEVICE
-                </Box>
-                <Box sx={{ cursor: "pointer" }} onClick={onClose}>
-                    <CloseIcon onClick={onClose} />
-                </Box>
-            </Box>
-            {/* Form  */}
-            <Box
+                  Device Type
+                </InputLabel>
+                <Select
+                  labelId="deviceType-label"
+                  id="deviceType"
+                  name="deviceType"
+                  value={values.deviceType}
+                  onBlur={handleBlur}
+                  onChange={handleChange}
+                >
+                  <MenuItem value="gps">GPS</MenuItem>
+                  <MenuItem value="video">Video</MenuItem>
+                </Select>
+                {touched.deviceType && errors.deviceType && (
+                  <FormHelperText>{errors.deviceType}</FormHelperText>
+                )}
+              </FormControl>
+            </Grid>
+            <Grid item xs="12" lg="6">
+              <TextField
+                fullWidth
+                type="text"
+                label="Unique Id"
+                maxLength="30"
+                name="uniqueId"
+                id="uniqueId"
+                value={values.uniqueId}
+                onBlur={handleBlur}
+                onChange={handleChange}
+                error={touched.uniqueId && Boolean(errors.uniqueId)}
+                helperText={touched.uniqueId && errors.uniqueId}
+              />
+            </Grid>
+            <Grid item xs="12" lg="6">
+              <TextField
+                fullWidth
+                type="text"
+                label="Ip Address"
+                maxLength="30"
+                name="ipAddress"
+                id="ipAddress"
+                value={values.ipAddress}
+                onBlur={handleBlur}
+                onChange={handleChange}
+                error={touched.ipAddress && Boolean(errors.ipAddress)}
+                helperText={touched.ipAddress && errors.ipAddress}
+              />
+            </Grid>
+            {values.deviceType == "video" && (
+              <Grid item xs="12" lg="12">
+                <TextField
+                  fullWidth
+                  type="text"
+                  label="Url"
+                  maxLength="30"
+                  name="url"
+                  id="url"
+                  value={values.url}
+                  onBlur={handleBlur}
+                  onChange={handleChange}
+                  error={touched.url && Boolean(errors.url)}
+                  helperText={touched.url && errors.url}
+                />
+              </Grid>
+            )}
+            <Grid item xs="12" mt={3}>
+              <Box
                 sx={{
-                    marginTop: {
-                        xs: "1rem",
-                        lg: "2.5rem",
+                  display: "flex",
+                  alignItems: "center",
+                  gap: "1rem",
+                  width: "255px",
+                  justifyContent: "center",
+                }}
+              >
+                <CancelBtn onClick={onClose}>Cancel</CancelBtn>
+                <Button
+                  type="submit"
+                  startIcon={<SaveIcon />}
+                  sx={{
+                    color: "#fff",
+                    borderRadius: "16px",
+                    width: "157px",
+                    padding: "16px",
+                    "&:disabled": {
+                      opacity: "0.3",
+                      color: "white",
+                      cursor: "not-allowed",
                     },
-                }}
-            >
-                <Typography
-                    sx={{
-                        fontWeight: 700,
-                        fontSize: "20px",
-                        marginBottom: "2rem",
-                    }}
+                  }}
+                  // disabled={isLoading}
                 >
-                    General Info
-                </Typography>
-                <form onSubmit={handleSubmit}>
-                    <Grid container spacing="14">
-                        <Grid item xs="12" lg="6">
-                            <TextField
-                                fullWidth
-                                type="text"
-                                name="deviceName"
-                                value={values.deviceName}
-                                onBlur={handleBlur}
-                                onChange={handleChange}
-                                error={touched.deviceName && Boolean(errors.deviceName)}
-                                helperText={touched.deviceName && errors.deviceName}
-                                label="Device Name"
-                                maxLength="20"
-                            />
-                        </Grid>
-                        <Grid item xs="12" lg="6">
-                            <TextField
-                                fullWidth
-                                type="text"
-                                label="Device Type"
-                                maxLength="30"
-                                name="deviceType"
-                                id="deviceType"
-                                value={values.deviceType}
-                                onBlur={handleBlur}
-                                onChange={handleChange}
-                                error={touched.deviceType && Boolean(errors.deviceType)}
-                                helperText={touched.deviceType && errors.deviceType}
-                            />
-                        </Grid>
-                        <Grid item xs="12" lg="6">
-                            <TextField
-                                fullWidth
-                                type="text"
-                                label="Unique Id"
-                                maxLength="30"
-                                name="uniqueId"
-                                id="uniqueId"
-                                value={values.uniqueId}
-                                onBlur={handleBlur}
-                                onChange={handleChange}
-                                error={touched.uniqueId && Boolean(errors.uniqueId)}
-                                helperText={touched.uniqueId && errors.uniqueId}
-                            />
-                        </Grid>
-                        <Grid item xs="12" lg="6">
-                            <TextField
-                                fullWidth
-                                type="text"
-                                label="Ip Address"
-                                maxLength="30"
-                                name="ipAddress"
-                                id="ipAddress"
-                                value={values.ipAddress}
-                                onBlur={handleBlur}
-                                onChange={handleChange}
-                                error={touched.ipAddress && Boolean(errors.ipAddress)}
-                                helperText={touched.ipAddress && errors.ipAddress}
-                            />
-                        </Grid>
-                        <Grid item xs="12" mt={3}>
-                            <Box
-                                sx={{
-                                    display: "flex",
-                                    alignItems: "center",
-                                    gap: "1rem",
-                                    width: "255px",
-                                    justifyContent: "center",
-                                }}
-                            >
-                                <CancelBtn onClick={onClose}>Cancel</CancelBtn>
-                                <Button
-                                    type="submit"
-                                    startIcon={<SaveIcon />}
-                                    sx={{
-                                        color: "#fff",
-                                        borderRadius: "16px",
-                                        width: "157px",
-                                        padding: "16px",
-                                        "&:disabled": {
-                                            opacity: "0.3",
-                                            color: "white",
-                                            cursor: "not-allowed",
-                                        },
-                                    }}
-                                    // disabled={isLoading}
-                                >
-                                    {/* {isLoading ? "Saving..." : "SAVE Device"} */}
-                                    Save Device
-                                </Button>
-                            </Box>
-                        </Grid>
-                    </Grid>
-                </form>
-            </Box>
-        </Fragment>
-    );
+                  {/* {isLoading ? "Saving..." : "SAVE Device"} */}
+                  Save Device
+                </Button>
+              </Box>
+            </Grid>
+          </Grid>
+        </form>
+      </Box>
+    </Fragment>
+  );
 };
 
 export default AddDevice;
 
 const CancelBtn = styled("span")({
-    fontsize: "16px",
-    fontWeight: 600,
-    color: "rgba(17, 17, 17, 1)",
-    cursor: "pointer",
+  fontsize: "16px",
+  fontWeight: 600,
+  color: "rgba(17, 17, 17, 1)",
+  cursor: "pointer",
 });
--- src/pages/dashboard/settings/devices/components/EditDevice.jsx
+++ src/pages/dashboard/settings/devices/components/EditDevice.jsx
@@ -1,5 +1,16 @@
 /* eslint-disable react/prop-types */
-import { Box, Button, Grid, TextField, Typography, styled } from "@mui/material";
+import {
+  Box,
+  Button,
+  FormControl,
+  Grid,
+  InputLabel,
+  MenuItem,
+  Select,
+  TextField,
+  Typography,
+  styled,
+} from "@mui/material";
 import React, { useState } from "react";
 import BackIcon from "../../../../../assets/svgs/modal/BackIcon";
 import CloseIcon from "../../../../../assets/svgs/modal/CloseIcon";
@@ -9,154 +20,192 @@ import { updateDeviceAction } from "../../../../../redux/actions/device.actions"
 import { toast } from "react-toastify";
 
 const EditDevice = ({ onClose, device }) => {
-    const dispatch = useDispatch();
-    const [deviceName, setDeviceName] = useState(device?.name);
-    const [deviceType, setDeviceType] = useState(device?.type);
+  const dispatch = useDispatch();
+  const [deviceName, setDeviceName] = useState(device?.name);
+  const [deviceType, setDeviceType] = useState(device?.type);
+  const [deviceUrl, setDeviceUrl] = useState(device?.url);
+  const [deviceIp, setDeviceIp] = useState(device?.ip);
+  const [deviceUniqueId, setDeviceUniqueId] = useState(device?.uniqueId);
+  const [isLoading, setIsLoading] = useState(false);
 
-    const handleSaveEditDevice = () => {
-        const data = {};
-        if (deviceName && deviceName != device?.name) {
-            data.name = deviceName;
-        }
-        if (deviceType && deviceType != device?.type) {
-            data.type = deviceType;
-        }
-        if (!Object.keys(data).length) return toast.error("You have not made any changes");
-        dispatch(updateDeviceAction(device._id, data));
-    };
-    return (
-        <React.Fragment>
+  const handleSaveEditDevice = async () => {
+    try {
+      setIsLoading(true);
+      const data = {};
+      if (deviceName && deviceName != device?.name) data.name = deviceName;
+      if (deviceType && deviceType != device?.type) data.type = deviceType;
+      if (deviceIp && deviceIp != device?.ip) data.ip = deviceIp;
+      if (deviceUniqueId && deviceUniqueId != device?.uniqueId) data.uniqueId = deviceUniqueId;
+      if (deviceType == "video" && deviceUrl && deviceUrl != device?.url) data.url = deviceUrl;
+      else if (deviceType !== "video" && deviceUrl) {
+        data.url = "remove";
+      }
+      if (!Object.keys(data).length) {
+        setIsLoading(false);
+        return toast.error("You have not made any changes");
+      }
+      await dispatch(updateDeviceAction(device._id, data));
+      setIsLoading(false);
+    } catch (error) {
+      setIsLoading(false);
+    }
+  };
+  return (
+    <React.Fragment>
+      <Box
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "space-between",
+        }}
+      >
+        <Box
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            gap: "1rem",
+            color: "rgba(17, 17, 17, 1)",
+            fontSize: {
+              xs: "1rem",
+              md: "1.5rem",
+            },
+            fontWeight: 600,
+          }}
+        >
+          <Box sx={{ cursor: "pointer", height: "25px" }} onClick={onClose}>
+            <BackIcon />
+          </Box>
+          EDIT DEVICE
+        </Box>
+        <Box sx={{ cursor: "pointer" }} onClick={onClose}>
+          <CloseIcon onClick={onClose} />
+        </Box>
+      </Box>
+      {/* Form  */}
+      <Box
+        sx={{
+          marginTop: {
+            xs: "1rem",
+            lg: "2.5rem",
+          },
+        }}
+      >
+        <Typography
+          sx={{
+            fontWeight: 700,
+            fontSize: "20px",
+            marginBottom: "2rem",
+          }}
+        >
+          General Info
+        </Typography>
+        <Grid container spacing="14">
+          <Grid item xs="12" lg="6">
+            <TextField
+              fullWidth
+              type="text"
+              label="Device Name"
+              maxLength="20"
+              value={deviceName}
+              onChange={(e) => setDeviceName(e.target.value)}
+            />
+          </Grid>
+
+          <Grid item xs="12" lg="6">
+            <TextField
+              fullWidth
+              type="text"
+              label="Unique Id"
+              maxLength="30"
+              value={deviceUniqueId}
+              onChange={(e) => setDeviceUniqueId(e.target.value)}
+            />
+          </Grid>
+          <Grid item xs="12" lg="6">
+            <FormControl fullWidth>
+              <InputLabel
+                sx={{ backgroundColor: "whitesmoke", width: "fit-content", px: 1 }}
+                id="deviceType-label"
+              >
+                Device Type
+              </InputLabel>
+              <Select
+                labelId="deviceType-label"
+                id="deviceType"
+                name="deviceType"
+                value={deviceType}
+                onChange={(e) => setDeviceType(e.target.value)}
+              >
+                <MenuItem value="gps">GPS</MenuItem>
+                <MenuItem value="video">Video</MenuItem>
+              </Select>
+            </FormControl>
+          </Grid>
+          <Grid item xs="12" lg="6">
+            <TextField
+              fullWidth
+              type="text"
+              label="Ip Address"
+              maxLength="30"
+              value={deviceIp}
+              onChange={(e) => setDeviceIp(e.target.value)}
+            />
+          </Grid>
+          {deviceType === "video" && (
+            <Grid item xs="12" lg="6">
+              <TextField
+                fullWidth
+                type="text"
+                label="Url"
+                maxLength="30"
+                id="url"
+                value={deviceUrl}
+                onChange={(e) => setDeviceUrl(e.target.value)}
+              />
+            </Grid>
+          )}
+          <Grid item xs="12" mt={3}>
             <Box
-                sx={{
-                    display: "flex",
-                    alignItems: "center",
-                    justifyContent: "space-between",
-                }}
+              sx={{
+                display: "flex",
+                alignItems: "center",
+                gap: "1rem",
+                width: "255px",
+                justifyContent: "center",
+              }}
             >
-                <Box
-                    sx={{
-                        display: "flex",
-                        alignItems: "center",
-                        gap: "1rem",
-                        color: "rgba(17, 17, 17, 1)",
-                        fontSize: {
-                            xs: "1rem",
-                            md: "1.5rem",
-                        },
-                        fontWeight: 600,
-                    }}
-                >
-                    <Box sx={{ cursor: "pointer", height: "25px" }} onClick={onClose}>
-                        <BackIcon />
-                    </Box>
-                    EDIT DEVICE
-                </Box>
-                <Box sx={{ cursor: "pointer" }} onClick={onClose}>
-                    <CloseIcon onClick={onClose} />
-                </Box>
-            </Box>
-            {/* Form  */}
-            <Box
+              <CancelBtn onClick={onClose}>Cancel</CancelBtn>
+              <Button
+                onClick={handleSaveEditDevice}
+                startIcon={<SaveIcon />}
                 sx={{
-                    marginTop: {
-                        xs: "1rem",
-                        lg: "2.5rem",
-                    },
+                  color: "#fff",
+                  borderRadius: "16px",
+                  width: "157px",
+                  padding: "16px",
+                  "&:disabled": {
+                    opacity: "0.3",
+                    color: "white",
+                    cursor: "not-allowed",
+                  },
                 }}
-            >
-                <Typography
-                    sx={{
-                        fontWeight: 700,
-                        fontSize: "20px",
-                        marginBottom: "2rem",
-                    }}
-                >
-                    General Info
-                </Typography>
-                <Grid container spacing="14">
-                    <Grid item xs="12" lg="6">
-                        <TextField
-                            fullWidth
-                            type="text"
-                            label="Device Name"
-                            maxLength="20"
-                            value={deviceName}
-                            onChange={(e) => setDeviceName(e.target.value)}
-                        />
-                    </Grid>
-                    {/* <Grid item xs="12" lg="6">
-                        <TextField
-                            fullWidth
-                            type="text"
-                            label="Device Type"
-                            maxLength="30"
-                            value={deviceType}
-                            onChange={(e) => setDeviceType(e.target.value)}
-                        />
-                    </Grid> */}
-                    <Grid item xs="12" lg="6">
-                        <TextField
-                            fullWidth
-                            type="text"
-                            label="Unique Id"
-                            maxLength="30"
-                            // value={uniqueId}
-                            // onChange={(e) => setDeviceType(e.target.value)}
-                        />
-                    </Grid>
-                    <Grid item xs="12" lg="6">
-                        <TextField
-                            fullWidth
-                            type="text"
-                            label="Ip Address"
-                            maxLength="30"
-                            // value={ipAddress}
-                            // onChange={(e) => setDeviceType(e.target.value)}
-                        />
-                    </Grid>
-                    <Grid item xs="12" mt={3}>
-                        <Box
-                            sx={{
-                                display: "flex",
-                                alignItems: "center",
-                                gap: "1rem",
-                                width: "255px",
-                                justifyContent: "center",
-                            }}
-                        >
-                            <CancelBtn onClick={onClose}>Cancel</CancelBtn>
-                            <Button
-                                onClick={handleSaveEditDevice}
-                                startIcon={<SaveIcon />}
-                                sx={{
-                                    color: "#fff",
-                                    borderRadius: "16px",
-                                    width: "157px",
-                                    padding: "16px",
-                                    "&:disabled": {
-                                        opacity: "0.3",
-                                        color: "white",
-                                        cursor: "not-allowed",
-                                    },
-                                }}
-                                // disabled={isLoading}
-                            >
-                                {/* {isLoading ? "Saving..." : "SAVE Device"} */}
-                                Save Device
-                            </Button>
-                        </Box>
-                    </Grid>
-                </Grid>
+                disabled={isLoading}
+              >
+                {isLoading ? "Saving..." : "Save Device"}
+              </Button>
             </Box>
-        </React.Fragment>
-    );
+          </Grid>
+        </Grid>
+      </Box>
+    </React.Fragment>
+  );
 };
 
 export default EditDevice;
 
 const CancelBtn = styled("span")({
-    fontsize: "16px",
-    fontWeight: 600,
-    color: "rgba(17, 17, 17, 1)",
-    cursor: "pointer",
+  fontsize: "16px",
+  fontWeight: 600,
+  color: "rgba(17, 17, 17, 1)",
+  cursor: "pointer",
 });
