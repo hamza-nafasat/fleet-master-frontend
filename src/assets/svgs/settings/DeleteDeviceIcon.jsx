6486988f26b18ecead6d86548d698100085d7ffd
Asif Zulfiqar <asifzulfiqar43@gmail.com>
2024-08-29 18:15:00 +0500

    truck detail page updated

---
+++ src/assets/svgs/settings/DeleteDeviceIcon.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+
+const DeleteDeviceIcon = () => {
+  return (
+    <>
+      <svg
+        width="25"
+        height="26"
+        viewBox="0 0 25 26"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M3.125 6.5H5.20833H21.875"
+          stroke="#FF0000"
+          strokeWidth="2.80315"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+        <path
+          d="M19.7943 6.49984V21.6665C19.7943 22.2411 19.5748 22.7922 19.1841 23.1986C18.7934 23.6049 18.2635 23.8332 17.7109 23.8332H7.29427C6.74174 23.8332 6.21183 23.6049 5.82113 23.1986C5.43043 22.7922 5.21094 22.2411 5.21094 21.6665V6.49984M8.33594 6.49984V4.33317C8.33594 3.75853 8.55543 3.20743 8.94613 2.80111C9.33683 2.39478 9.86674 2.1665 10.4193 2.1665H14.5859C15.1385 2.1665 15.6684 2.39478 16.0591 2.80111C16.4498 3.20743 16.6693 3.75853 16.6693 4.33317V6.49984"
+          stroke="#FF0000"
+          strokeWidth="2.10236"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+        <path
+          d="M10.416 11.9165V18.4165"
+          stroke="#FF0000"
+          strokeWidth="2.10236"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+        <path
+          d="M14.5859 11.9165V18.4165"
+          stroke="#FF0000"
+          strokeWidth="2.10236"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+      </svg>
+    </>
+  );
+};
+
+export default DeleteDeviceIcon;
--- src/pages/dashboard/settings/trucks/components/TruckDetail.jsx
+++ src/pages/dashboard/settings/trucks/components/TruckDetail.jsx
@@ -5,7 +5,7 @@ import { Avatar, Box, Grid, Tooltip, Typography, styled } from "@mui/material";
 import React, { useEffect, useState } from "react";
 import { confirmAlert } from "react-confirm-alert";
 import { useDispatch, useSelector } from "react-redux";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import { toast } from "react-toastify";
 import profilePic from "../../../../../assets/images/settings/vehicle-pic.png";
 import Modal from "../../../../../components/modal/Modal";
@@ -13,8 +13,20 @@ import {
   detachDeviceFromTruckAction,
   getSingleTruckAction,
 } from "../../../../../redux/actions/truck.actions";
-import { clearTruckError, clearTruckMessage } from "../../../../../redux/slices/truck.slice";
+import {
+  clearTruckError,
+  clearTruckMessage,
+} from "../../../../../redux/slices/truck.slice";
 import AttacheModal from "./AttacheModal";
+import EditIcon from "../../../../../assets/svgs/settings/EditIcon";
+import DeleteIcon from "../../../../../assets/svgs/settings/DeleteIcon";
+import DeleteDeviceIcon from "../../../../../assets/svgs/settings/DeleteDeviceIcon";
+import LocationIcon from "../../../../../assets/svgs/home/LocationIcon";
+import PlayIcon from "../../../../../assets/svgs/home/PlayIcon";
+import Profile from "../../../../../assets/images/asif.png";
+import EditTruck from "./EditTruck";
+import DetachIcon from "../../../../../assets/svgs/settings/DetachIcon";
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 
 const TruckDetail = () => {
   const dispatch = useDispatch();
@@ -28,10 +40,27 @@ const TruckDetail = () => {
     setTruckForAttach(truckId);
     setModalType("attach");
   };
+  const handleEditTruckModal = () => setModalType("edit-truck");
 
   const handleCloseModal = () => {
     setModalType(null);
   };
+
+  const deleteHandler = () =>
+    confirmAlert({
+      title: "Confirm delete Trucks",
+      message: "Are you sure you want to delete the Truck?",
+      buttons: [
+        {
+          label: "Yes",
+          onClick: console.log("deleted"),
+        },
+        {
+          label: "No",
+          // onClick: () => toast.info("Delete action cancelled", { autoClose: 2000 }),
+        },
+      ],
+    });
 
   useEffect(() => {
     if (message) {
@@ -48,42 +77,91 @@ const TruckDetail = () => {
   return (
     <React.Fragment>
       <TruckContainer container sx={{ padding: { xs: "16px", md: "24px" } }}>
-        <Grid item xs={11}>
-          <Avatar
-            alt="profile"
-            src={profilePic}
-            sx={{
-              width: "200px",
-              height: "200px",
-              position: "relative",
-              overflow: "hidden",
-              borderRadius: "50%",
-              objectFit: "cover",
-            }}
-          />
-          <Typography variant="h6" sx={{ width: "200px", textAlign: "center", mt: 1 }}>
-            {truck?.truckName}
-          </Typography>
-        </Grid>
-        <Grid item xs={1} display="flex" justifyContent="flex-end">
-          <Box sx={{ cursor: "pointer" }} onClick={() => handleOpenAttachModal(truck?._id)}>
-            <Tooltip title="Attach Device">
-              <AddLinkIcon style={{ color: "#006bce", width: "40px", height: "40px" }} />
-            </Tooltip>
+        <Grid xs={12} display="flex" justifyContent="space-between" mb={3}>
+          <Link to='/dashboard/setting/trucks'>
+            <ArrowBackIcon sx={{color:'rgba(70, 66, 85, 1)', fontSize:'24px'}} />
+          </Link>
+          <Box sx={{ display: "flex", alignItems: "center", gap: "1rem" }}>
+            <div style={{ cursor: "pointer" }} onClick={handleEditTruckModal}>
+              <EditIcon />
+            </div>
+            <div style={{ cursor: "pointer" }} onClick={deleteHandler}>
+              <DeleteDeviceIcon />
+            </div>
           </Box>
         </Grid>
-        <Grid item xs={12} mt={4}>
-          <Typography variant="h6">Attached Devices</Typography>
-          <Grid container spacing={2} mt={1}>
-            {truck?.devices?.map((device) => (
-              <DeviceCard key={device._id} device={device} truck={truck} />
-            ))}
+        <Grid
+          item
+          xs={12}
+          sx={{
+            boxShadow: "0px 0px 4px 0px rgba(0, 0, 0, 0.25);",
+            borderRadius: "10px",
+            padding: { xs: "10px", sm: "16px" },
+          }}
+          mb={3}
+        >
+          <Grid container spacing={2}>
+            <Grid
+              item
+              xs={12}
+              md={8}
+              display="flex"
+              justifyContent="space-between"
+              gap={1}
+            >
+              <SingleTruckDetail />
+            </Grid>
+            <Grid item xs={12} md={4}>
+              <UserProfile />
+            </Grid>
+          </Grid>
+        </Grid>
+        <Grid
+          container
+          sx={{
+            boxShadow: "0px 0px 4px 0px rgba(0, 0, 0, 0.25);",
+            borderRadius: "10px",
+            padding: { xs: "10px", sm: "16px" },
+          }}
+        >
+          <Grid item xs={12} display="flex" justifyContent="space-between">
+            <Typography
+              sx={{
+                color: "rgba(0, 68, 131, 1)",
+                fontWeight: 600,
+                fontSize: { xs: "16px", sm: "22px" },
+              }}
+            >
+              Devices
+            </Typography>
+            <Box
+              sx={{ cursor: "pointer" }}
+              onClick={() => handleOpenAttachModal(truck?._id)}
+            >
+              <Tooltip title="Attach Device">
+                <AddLinkIcon
+                  style={{ color: "#006bce", width: "40px", height: "40px" }}
+                />
+              </Tooltip>
+            </Box>
+          </Grid>
+          <Grid item xs={12}>
+            <Grid container spacing={2} mt={1}>
+              {truck?.devices?.map((device) => (
+                <DeviceCard key={device._id} device={device} truck={truck} />
+              ))}
+            </Grid>
           </Grid>
         </Grid>
       </TruckContainer>
       {modalType === "attach" && (
         <Modal onClose={handleCloseModal}>
           <AttacheModal onClose={handleCloseModal} truckId={truckForAttach} />
+        </Modal>
+      )}
+      {modalType === "edit-truck" && (
+        <Modal onClose={handleCloseModal}>
+          <EditTruck onClose={handleCloseModal} />
         </Modal>
       )}
     </React.Fragment>
@@ -102,8 +180,10 @@ const DeviceCard = ({ device, truck }) => {
         {
           label: "Yes",
           onClick: async () => {
-            if (!deviceId) return toast.info("Device not found", { autoClose: 2000 });
-            if (!truckId) return toast.info("Truck not found", { autoClose: 2000 });
+            if (!deviceId)
+              return toast.info("Device not found", { autoClose: 2000 });
+            if (!truckId)
+              return toast.info("Truck not found", { autoClose: 2000 });
             console.log("device id", deviceId, "truck id", truckId);
             await dispatch(detachDeviceFromTruckAction(truckId, deviceId));
           },
@@ -120,125 +200,254 @@ const DeviceCard = ({ device, truck }) => {
     <Grid item xs={12} md={4}>
       <Box
         sx={{
-          position: "relative",
-          background: "rgba(255, 255, 255, 1)",
-          borderRadius: "12px",
-          padding: {
-            xs: "1rem",
-            md: "2rem",
-          },
+          background: "rgba(238, 247, 255, 1)",
+          borderLeft: "5px solid rgba(255, 71, 71, 1)",
+          borderRadius: "0px, 14px, 14px, 0px",
+          padding: "16px",
         }}
       >
-        <Typography
-          sx={{
-            textAlign: "center",
-            color: "rgba(70, 66, 85, 1)",
-            fontSize: "18px",
-            fontWeight: 500,
-          }}
-        >
-          {device?._id}
-        </Typography>
         <Box
-          sx={{
-            marginTop: "1.5rem",
-            display: "flex",
-            justifyContent: "space-between",
-            gap: "1rem",
-          }}
+          sx={{ display: "flex", justifyContent: "space-between", gap: "1rem" }}
         >
           <Box>
-            <Typography
-              sx={{
-                color: "rgba(127, 127, 146, 1)",
-                fontSize: "12px",
-              }}
-            >
-              Creation Date
+            <Typography sx={{ color: "rgba(0, 107, 206, 1)" }}>
+              Camera245331
             </Typography>
             <Typography
               sx={{
-                color: "rgba(0, 107, 206, 1)",
+                color: "rgba(70, 66, 85, 1)",
+                fontWeight: 500,
                 fontSize: "16px",
               }}
             >
-              {device?.createdAt?.split("T")[0]}
+              Dashboard Cam
             </Typography>
           </Box>
-          <Box>
-            <Typography
-              sx={{
-                color: "rgba(127, 127, 146, 1)",
-                fontSize: "12px",
-              }}
-            >
-              Device Name
-            </Typography>
-            <Typography
-              sx={{
-                color: "rgba(0, 107, 206, 1)",
-                fontSize: "16px",
-              }}
-            >
-              {device?.name}
-            </Typography>
+          <Box
+            onClick={() => deleteDeviceHandle(truck._id, device._id)}
+            sx={{ cursor: "pointer" }}
+          >
+            <Tooltip title="Detach Device">
+              <DetachIcon />
+            </Tooltip>
           </Box>
         </Box>
-        <Box
-          sx={{
-            marginTop: "1.5rem",
-            display: "flex",
-            justifyContent: "space-between",
-            gap: "1rem",
-          }}
-        >
-          <Box>
-            <Typography
-              sx={{
-                color: "rgba(127, 127, 146, 1)",
-                fontSize: "12px",
-              }}
-            >
-              Device Type
+        <Grid container spacing={1} mt={2}>
+          <Grid item xs={6}>
+            <Typography sx={{ color: "rgba(127,127,146,1)", fontSize: "12px" }}>
+              URL
             </Typography>
             <Typography
-              sx={{
-                color: "rgba(0, 107, 206, 1)",
-                fontSize: "16px",
-              }}
+              sx={{ color: "rgba(0, 107, 206, 1)", fontSize: "14px" }}
             >
-              {device?.type}
+              http://sensors.example.com
             </Typography>
-          </Box>
-          <Box>
-            <Typography
-              sx={{
-                color: "rgba(127, 127, 146, 1)",
-                fontSize: "12px",
-              }}
-            >
-              Assigned To
+          </Grid>
+          <Grid
+            item
+            xs={6}
+            display="flex"
+            flexDirection="column"
+            alignItems="flex-end"
+          >
+            <Typography sx={{ color: "rgba(127,127,146,1)", fontSize: "12px" }}>
+              Type
             </Typography>
             <Typography
-              sx={{
-                color: "rgba(0, 107, 206, 1)",
-                fontSize: "16px",
-              }}
+              sx={{ color: "rgba(0, 107, 206, 1)", fontSize: "14px" }}
             >
-              {truck?.truckName}
+              Video
             </Typography>
-          </Box>
-        </Box>
-        <Box
-          onClick={() => deleteDeviceHandle(truck._id, device._id)}
-          sx={{ position: "absolute", top: 8, right: 8, cursor: "pointer" }}
-        >
-          <Tooltip title="Detach Device">
-            <CancelIcon style={{ color: "#d30000" }} />
-          </Tooltip>
-        </Box>
+          </Grid>
+        </Grid>
       </Box>
     </Grid>
+  );
+};
+
+const SingleTruckDetail = () => {
+  return (
+    <Grid container spacing={2}>
+      <Grid item xs={12} md={7}>
+        <Box
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "space-between",
+            gap: "1rem",
+            mb: 1,
+          }}
+        >
+          <Typography
+            sx={{
+              color: "rgba(0,68,131)",
+              fontWeight: 600,
+              fontSize: { xs: "18px", sm: "24px", md: "26px" },
+            }}
+          >
+            Land Cruiser V8
+          </Typography>
+          <Box sx={{ display: "flex", gap: "1rem" }}>
+            <div style={{ cursor: "pointer" }}>
+              <LocationIcon />
+            </div>
+            <div style={{ cursor: "pointer" }}>
+              <PlayIcon />
+            </div>
+          </Box>
+        </Box>
+        {truckDetailList.map((list, i) => (
+          <Box key={i}>
+            <TruckDataList title="Plate Number" value={list.plateNumber} />
+            <TruckDataList title="Fleet Number" value={list.fleetNumber} />
+            <TruckDataList title="Status" value={list.status} />
+            <TruckDataList title="Last Update" value={list.lastUpdate} />
+            <TruckDataList title="Created At" value={list.createdAt} />
+          </Box>
+        ))}
+      </Grid>
+      <Grid item xs={12} md={5}>
+        <Box
+          sx={{
+            background: "rgba(237, 237, 237, 1)",
+            borderRadius: "10px",
+            padding: "16px",
+            display: "grid",
+            placeItems: "center",
+            height: "100%",
+          }}
+        >
+          <img
+            src={profilePic}
+            alt="truck image"
+            style={{
+              objectFit: "cover",
+              width: "200px",
+              height: "200px",
+              borderRadius: "50%",
+            }}
+          />
+        </Box>
+      </Grid>
+    </Grid>
+  );
+};
+
+const TruckDataList = ({ title, value }) => {
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "space-between",
+        gap: "1rem",
+        py: 1,
+        borderBottom: "0.4px solid rgba(0, 0, 0, 0.3)",
+      }}
+    >
+      <Typography
+        sx={{
+          color: "rgba(70, 66, 85, 1)",
+          fontWeight: 500,
+          fontSize: { xs: "14px", sm: "16px" },
+        }}
+      >
+        {title}
+      </Typography>
+      <Typography
+        sx={{
+          color:
+            value === "Connected"
+              ? "rgba(47, 239, 64, 1)"
+              : "rgba(0, 107, 206, 1)",
+          fontWeight: 500,
+          fontSize: { xs: "14px", sm: "16px" },
+        }}
+      >
+        {value}
+      </Typography>
+    </Box>
+  );
+};
+
+const UserProfile = () => {
+  return (
+    <Box
+      sx={{
+        height: "100%",
+        background: "rgba(238, 247, 255, 1)",
+        borderRadius: "10px",
+        padding: { xs: "8px", sm: "16px" },
+      }}
+    >
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          gap: "0.5rem",
+          alignItems: "center",
+          mb: 1,
+        }}
+      >
+        <Avatar
+          src={Profile}
+          sx={{
+            width: "102px",
+            height: "102px",
+            border: "2px solid rgba(0, 107, 206, 1)",
+          }}
+        />
+        <Typography
+          sx={{
+            color: "rgba(0, 107, 206, 1)",
+            fontWeight: 600,
+            fontSize: { xs: "16px", md: "20px" },
+          }}
+        >
+          Asif Zulfiqar
+        </Typography>
+      </Box>
+      {userDetails.map((list, i) => (
+        <Box key={i}>
+          <UserList title="Email" value={list.email} />
+          <UserList title="Number" value={list.number} />
+          <UserList title="License Expiry " value={list.licenseExpiry} />
+          <UserList title="Last Update" value={list.lastUpdate} />
+        </Box>
+      ))}
+    </Box>
+  );
+};
+
+const UserList = ({ title, value }) => {
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "space-between",
+        gap: "1rem",
+        mt: 1,
+      }}
+    >
+      <Typography
+        sx={{
+          color: "rgba(70, 66, 85, 1)",
+          fontWeight: 500,
+          fontSize: { xs: "14px", sm: "16px" },
+        }}
+      >
+        {title}
+      </Typography>
+      <Typography
+        sx={{
+          color: "rgba(0, 107, 206, 1)",
+          fontSize: "14px",
+        }}
+      >
+        {value}
+      </Typography>
+    </Box>
   );
 };
 
@@ -248,3 +457,22 @@ const TruckContainer = styled(Grid)({
   // height: "100%",
   marginTop: "-4rem",
 });
+
+var truckDetailList = [
+  {
+    plateNumber: "XYZ-1235",
+    fleetNumber: "Flt-23s3",
+    status: "Connected",
+    lastUpdate: "05-24-2024",
+    createdAt: "05-24-2024",
+  },
+];
+
+var userDetails = [
+  {
+    email: "asifzulfiqar@gmail.com",
+    number: "+92 334 2342342",
+    licenseExpiry: "05-24-2024",
+    lastUpdate: "05-24-2024",
+  },
+];
