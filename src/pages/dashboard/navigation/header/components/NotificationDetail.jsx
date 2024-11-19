317628dc5a6fb8ca56d2a05f4f1c04544b8783ff
hamza-nafasat <hamzanafasat4155@gmail.com>
2024-11-19 12:11:27 +0500

    add updated code for notification changes

--- src/pages/dashboard/navigation/header/components/NotificationDetail.jsx
+++ src/pages/dashboard/navigation/header/components/NotificationDetail.jsx
@@ -16,18 +16,11 @@ import {
   readAllNotificationsAction,
 } from "../../../../../redux/actions/notification.actions";
 import NoData from "../../../../../components/noData/NoData";
-<<<<<<< Updated upstream
-import { IoCheckmarkOutline } from "react-icons/io5";
-import { IoCheckmarkDoneOutline } from "react-icons/io5";
-=======
->>>>>>> Stashed changes
 
 const NotificationDetail = () => {
   const dispatch = useDispatch();
   const [isDelLoading, setIsDelLoading] = useState(false);
   const [rows, setRows] = useState([]);
-  const [page, setPage] = useState(0); // Note: DataGrid uses 0-based index for pagination
-  const [pageSize, setPageSize] = useState(12); // Rows per page
   const { notifications } = useSelector((state) => state.notification);
 
   const handleDeleteList = async (row) => {
@@ -80,6 +73,7 @@ const NotificationDetail = () => {
       align: "center",
       width: 200,
     },
+
     {
       field: "operation",
       headerName: "OPERATION",
@@ -87,76 +81,37 @@ const NotificationDetail = () => {
       headerAlign: "center",
       width: 180,
       renderCell: (params) => (
-<<<<<<< Updated upstream
-        <Box
-          sx={{
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            height: "100%",
-            gap: 2,
-          }}
-        >
-          <DeleteIcon
-            onClick={() => handleDeleteList(params.row)}
-            isLoading={isDelLoading}
-          />
-=======
         <Box sx={{ display: "flex", justifyContent: "center", alignItems: "center", height: "100%", gap: 3 }}>
           <DeleteIcon onClick={() => handleDeleteList(params.row)} isLoading={isDelLoading} />
->>>>>>> Stashed changes
           <Link
-            style={{
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-            }}
+            style={{ display: "flex", justifyContent: "center", alignItems: "center" }}
             to={`/dashboard/truck-detail/${params?.row?.truckId}`}
           >
             <FcViewDetails style={{ fontSize: "1.8rem", cursor: "pointer" }} />
-          </Link>
-          <Link>
-            <IoCheckmarkOutline
-              style={{
-                fontSize: "1.6rem",
-                cursor: "pointer",
-              }}
-            />
-            {/* In case of read */}
-            {/* <IoCheckmarkDoneOutline
-              style={{
-                fontSize: "1.6rem",
-                cursor: "pointer",
-                color: "#75beff",
-              }}
-            /> */}
           </Link>
         </Box>
       ),
     },
   ];
-
   useEffect(() => {
     if (notifications) {
       setRows(
-        notifications.map((notification) => ({
-          ...notification,
-          id: notification._id,
-          type: notification.type,
-          message: notification.message,
-          createdAt:
-            notification.createdAt
-              .split("T")[0]
-              .split("-")
-              .reverse()
-              .join("-") +
-            "  at  " +
-            new Date(notification.createdAt).toLocaleString("en-US", {
-              hour: "numeric",
-              minute: "numeric",
-              hour12: true,
-            }),
-        }))
+        notifications.map((notification) => {
+          return {
+            ...notification,
+            id: notification._id,
+            type: notification.type,
+            message: notification.message,
+            createdAt:
+              notification.createdAt.split("T")[0].split("-").reverse().join("-") +
+              "  at  " +
+              new Date(notification.createdAt).toLocaleString("en-US", {
+                hour: "numeric",
+                minute: "numeric",
+                hour12: true,
+              }),
+          };
+        })
       );
     }
   }, [notifications]);
@@ -172,15 +127,6 @@ const NotificationDetail = () => {
   useEffect(() => {
     enterInPage();
   }, [enterInPage]);
-
-  const handlePageChange = (params) => {
-    setPage(params.page);
-  };
-
-  const handlePageSizeChange = (params) => {
-    setPageSize(params.pageSize);
-  };
-
   return (
     <Box
       sx={{
@@ -201,27 +147,7 @@ const NotificationDetail = () => {
       >
         <DownloadIcon />
       </Box>
-<<<<<<< Updated upstream
       {notifications.length > 0 ? (
-        <div style={{ height: "70%", width: "100%" }}>
-          <DataGrid
-            rows={rows}
-            columns={columns}
-            pagination
-            paginationMode="client"
-            page={page}
-            pageSize={pageSize}
-            onPageChange={handlePageChange}
-            onPageSizeChange={handlePageSizeChange}
-            rowCount={rows.length}
-            rowsPerPageOptions={[5, 10, 12, 20, 50]}
-            disableColumnMenu
-            disableSelectionOnClick
-            sx={{
-              "& .MuiDataGrid-row.even-row": {
-                backgroundColor: "#fafafa",
-=======
-      {notifications?.length > 0 ? (
         <DataGrid
           rows={rows}
           columns={columns}
@@ -241,65 +167,55 @@ const NotificationDetail = () => {
               fontSize: {
                 xs: "14px",
                 md: "16px",
->>>>>>> Stashed changes
               },
-              "& .MuiDataGrid-columnHeader .MuiDataGrid-columnHeaderTitle": {
-                fontSize: {
-                  xs: "14px",
-                  md: "16px",
-                },
-                fontWeight: 600,
-                color: "#111111",
+              fontWeight: 600,
+              color: "#111111",
+            },
+            "& .MuiDataGrid-row .MuiDataGrid-cell": {
+              fontSize: {
+                xs: "14px",
+                md: "16px",
               },
-              "& .MuiDataGrid-row .MuiDataGrid-cell": {
-                fontSize: {
-                  xs: "14px",
-                  md: "16px",
-                },
-                background: "#fafafa",
-                fontWeight: 400,
-                color: "rgba(17, 17, 17, 0.6)",
+              background: "#fafafa",
+              fontWeight: 400,
+              color: "rgba(17, 17, 17, 0.6)",
+            },
+            "& .MuiDataGrid-root": {
+              borderTopLeftRadius: "24px !important",
+              borderTopRightRadius: "24px !important",
+              border: "0 !important",
+              overflow: "hidden",
+              width: "100%",
+            },
+            "& .MuiDataGrid-main": {
+              borderTopLeftRadius: "24px",
+              borderTopRightRadius: "24px",
+              width: "100%",
+              padding: "0 10px",
+            },
+            "& .MuiDataGrid-overlay": {
+              borderTopLeftRadius: "24px",
+              borderTopRightRadius: "24px",
+            },
+            "& .MuiDataGrid-footerContainer": {
+              display: "none",
+            },
+            "& .MuiDataGrid-scrollbar": {
+              "&::-webkit-scrollbar": {
+                width: "6px",
+                height: "6px",
               },
-              "& .MuiDataGrid-root": {
-                borderTopLeftRadius: "24px !important",
-                borderTopRightRadius: "24px !important",
-                border: "0 !important",
-                overflow: "hidden",
-                width: "100%",
+              "&::-webkit-scrollbar-track": {
+                background: "#00193333",
+                borderRadius: "6px",
               },
-              "& .MuiDataGrid-main": {
-                borderTopLeftRadius: "24px",
-                borderTopRightRadius: "24px",
-                width: "100%",
-                padding: "0 10px",
+              "&::-webkit-scrollbar-thumb": {
+                background: "#006bce",
+                borderRadius: "10px",
               },
-<<<<<<< Updated upstream
-              "& .MuiDataGrid-overlay": {
-                borderTopLeftRadius: "24px",
-                borderTopRightRadius: "24px",
-              },
-              "& .MuiDataGrid-scrollbar": {
-                "&::-webkit-scrollbar": {
-                  width: "6px",
-                  height: "6px",
-                },
-                "&::-webkit-scrollbar-track": {
-                  background: "#00193333",
-                  borderRadius: "6px",
-                },
-                "&::-webkit-scrollbar-thumb": {
-                  background: "#006bce",
-                  borderRadius: "10px",
-                },
-              },
-            }}
-          />
-        </div>
-=======
             },
           }}
         />
->>>>>>> Stashed changes
       ) : (
         <NoData />
       )}
