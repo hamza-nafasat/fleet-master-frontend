1318e0f888e846de3022f792fb6a8255b1fae235
hamza-nafasat <hamzanafasat4155@gmail.com>
2025-03-01 18:14:59 +0500

    complete new parameters requrements

--- src/config/getEnv.js
+++ src/config/getEnv.js
@@ -1,9 +1,7 @@
 const getEnv = (key) => {
-    const value = import.meta.env[key];
-    if (!value) {
-        throw new Error(`Environment variable ${key} is not set`);
-    }
-    return value;
+  const value = import.meta.env[key];
+  if (!value) throw new Error(`Environment variable ${key} is not set`);
+  return value;
 };
 
 export default getEnv;
